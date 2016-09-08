7d0320538b17d3a0469295c0d79f817b7a643e77
Richard Astbury <richard.astbury@gmail.com>
2016-09-08 04:51:52 -0700

    fix linebreaks on linux

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var countries = [];
 
 function load(){
 	var path = require('path');
-	var data = require('fs').readFileSync(path.join(__dirname, 'country-codes.csv')).toString().split('\r\n');
-		
+	var data = require('fs').readFileSync(path.join(__dirname, 'country-codes.csv')).toString().split('\n');
+
 	var headings = data[0].split(',');
 
 	for (var i = 1; i < data.length; i++){
 		var parts = data[i].split(',');
 		var country = {};
 		for (var j = 0; j < headings.length; j++){
-			country[headings[j]] = parts[j];
+			country[headings[j]] = parts[j].trim();
 		}
 		countries.push(country);
 	}
@@ -26,15 +26,15 @@ module.exports.byFips = function(code){
 
 module.exports.byIso = function(code){
 	if (!isNaN(parseInt(code))){
-		return search('isoNo', code.toString());	
+		return search('isoNo', code.toString());
 	}
 
 	if (code.length === 2){
-		return search('iso2', code.toUpperCase());	
-	} 
-	
+		return search('iso2', code.toUpperCase());
+	}
+
 	if (code.length === 3){
-		return search('iso3', code.toUpperCase());	
+		return search('iso3', code.toUpperCase());
 	} 
 
 	throw new Error("cannot determine ISO code type");
