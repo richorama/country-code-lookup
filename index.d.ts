5969e6314d04d6e8991dc5d07bf4bbb0a43f98c5
getlarge <ed@getlarge.eu>
2021-02-17 18:06:40 +0100

    add types declaration

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+export interface Country {
+  continent: string;
+  region: string;
+  country: string;
+  capital: string;
+  fips: string;
+  iso2: string;
+  iso3: string;
+  isoNo: string;
+  internet: string;
+}
+
+export type SearchOutput = Country | null;
+
+export const countries: Country[];
+
+declare function search(field: string, code: string | number): SearchOutput;
+
+export function byFips(code: string): SearchOutput;
+
+export function byIso(code: string | number): SearchOutput;
+
+export function byInternet(code: string): SearchOutput;
+
+export function byCountry(country: string): SearchOutput;
